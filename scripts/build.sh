f7fe9865af41270b93b3a6c2fa2d04e1344ea63b
Chris de Bruin <chris@flexcoder.nl>
2019-05-04 19:37:06 +0200

    use caching for docker build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
-docker build --build-arg RAILS_MASTER_KEY=$(cat config/master.key) -t rails6 .
-docker tag rails6:latest chrisdebruin/rails6
-docker push chrisdebruin/rails6
+export CACHE_IMAGE=chrisdebruin/rails6
+
+docker pull $CACHE_IMAGE:$BRANCH_NAME || true
+docker build --cache-from $CACHE_IMAGE:$BRANCH_NAME --build-arg RAILS_MASTER_KEY=$(cat config/master.key) --tag $CACHE_IMAGE:$BRANCH_NAME .
+
+docker push $CACHE_IMAGE:$BRANCH_NAME
