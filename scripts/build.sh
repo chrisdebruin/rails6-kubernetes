f5a1a1cce204ed11b9d6308511dd9c2ce6021eda
Chris de Bruin <chris@flexcoder.nl>
2019-05-08 20:33:30 +0200

    oeps

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,4 @@ export RAILS_ENV=staging
 docker pull $CACHE_IMAGE:$BRANCH_NAME || true
 docker build --cache-from $CACHE_IMAGE:$BRANCH_NAME --build-arg RAILS_MASTER_KEY=$(cat config/master.key) --build-arg RAILS_ENV=$RAILS_ENV --build-arg GIT_REVISION=$REVISION --tag $CACHE_IMAGE:$BRANCH_NAME .
 
-# docker push $CACHE_IMAGE:$BRANCH_NAME
+docker push $CACHE_IMAGE:$BRANCH_NAME
