a09595ff5eed677ad63a4946fbd9c2b0823be04c
Chris de Bruin <chris@flexcoder.nl>
2019-05-09 19:53:34 +0200

    fix build flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,6 @@ if [[ "${CI}" ]]; then
 fi
 
 docker pull $CACHE_IMAGE:$BRANCH_NAME || true
-docker build --quite --cache-from $CACHE_IMAGE:$BRANCH_NAME --build-arg RAILS_MASTER_KEY=$(cat config/master.key) --build-arg RAILS_ENV=$RAILS_ENV --build-arg GIT_REVISION=$REVISION --tag $CACHE_IMAGE:$BRANCH_NAME .
+docker build --quiet --cache-from $CACHE_IMAGE:$BRANCH_NAME --build-arg RAILS_MASTER_KEY=$(cat config/master.key) --build-arg RAILS_ENV=$RAILS_ENV --build-arg GIT_REVISION=$REVISION --tag $CACHE_IMAGE:$BRANCH_NAME .
 
 docker push $CACHE_IMAGE:$BRANCH_NAME
