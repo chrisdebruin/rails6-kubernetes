8e333067173973ad3b80ed0a9b3689ca2dc5df92
Chris de Bruin <chris@flexcoder.nl>
2019-05-28 09:50:31 +0200

    setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,8 @@
+kubectl create namespace staging-env
+
+kubectl config set-context --current --namespace=staging-env
+
+kubectl create secret generic rails-secrets --from-literal master-key=$(cat config/master.key)
+kubectl create secret tls flexcoder-secret --key certbot/live/k8s.flexcoder.nl/privkey.pem --cert certbot/live/k8s.flexcoder.nl/cert.pem
+
+kubectl apply -f config/k8s/
